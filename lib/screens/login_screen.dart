91c9da847e357fd5be2a48dd60c994c1cfe799c4
nourhanhelmy1 <nourhanhelmy01@outlook.com>
2023-03-20 00:48:15 +0200

    Added validations to signup form
    -implemented validation on all login and signup form inputs to make sure they are not empty and of required type
    -implemented validation on email input format and password length

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,180 +1,249 @@
-// // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+/// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-// import 'package:flutter/material.dart';
-// import 'signup_screen.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
-// import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:flutter/material.dart';
+import 'signup_screen.dart';
+import 'tabs_screen.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
-// // ignore: use_key_in_widget_constructors
-// class LoginScreen extends StatefulWidget {
-//   static const routeName = '/login';
+// ignore: use_key_in_widget_constructors
+class LoginScreen extends StatefulWidget {
+  static const routeName = '/login';
 
-//   @override
-//   // ignore: library_private_types_in_public_api
-//   _LoginScreenState createState() => _LoginScreenState();
-// }
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoginScreenState createState() => _LoginScreenState();
+}
 
-// class _LoginScreenState extends State<LoginScreen> {
-//   final TextEditingController _emailController = TextEditingController();
-//   final TextEditingController _passwordController = TextEditingController();
+class _LoginScreenState extends State<LoginScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isPasswordHidden = true; // added variable to track password visibility
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Eventbrite'),
-//         centerTitle: true,
-//       ),
-//       body: Container(
-//         padding: EdgeInsets.all(20),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.start,
-//           crossAxisAlignment: CrossAxisAlignment.center,
-//           children: [
-//             //'Let's get started' Container
-//             Container(
-//               alignment: Alignment.topLeft,
-//               padding: EdgeInsetsDirectional.only(top: 20, bottom: 10),
-//               child: Text(
-//                 'Let\'s get started',
-//                 style: TextStyle(
-//                   color: Colors.black,
-//                   fontSize: 40,
-//                   fontWeight: FontWeight.bold,
-//                 ),
-//               ),
-//             ),
+  //Email validation function (to check that it's not empty and in the right format)
+  String? emailValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter an email';
+    }
+    final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    if (!emailRegex.hasMatch(value)) {
+      return 'Please enter a valid email';
+    }
+    return null;
+  }
 
-//             //miniText Container
-//             Container(
-//               alignment: Alignment.topLeft,
-//               padding: EdgeInsetsDirectional.only(bottom: 50, end: 100),
-//               child: Text(
-//                 'Sign up or log in to see what\'s happening near you',
-//                 style: TextStyle(
-//                   color: Colors.black,
-//                   fontSize: 18,
-//                 ),
-//               ),
-//             ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text('Eventbrite'),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  //'Let's get started' Container
+                  Container(
+                    alignment: Alignment.topLeft,
+                    padding:
+                        const EdgeInsetsDirectional.only(top: 20, bottom: 10),
+                    child: const Text(
+                      'Let\'s get started',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
 
-//             //Text fields for entering email and passowrd
-//             TextField(
-//               controller: _emailController,
-//               decoration: InputDecoration(
-//                 labelText: 'Email',
-//               ),
-//             ),
-//             TextField(
-//               controller: _passwordController,
-//               decoration: InputDecoration(
-//                 labelText: 'Password',
-//               ),
-//               obscureText: true,
-//             ),
-//             SizedBox(height: 20),
-//             ElevatedButton(
-//               onPressed: () {
-//                 //Implement login API call
-//               },
-//               child: Text('LOGIN'),
-//             ),
-//             SizedBox(height: 20),
+                  //miniText Container
+                  Container(
+                    alignment: Alignment.topLeft,
+                    padding:
+                        const EdgeInsetsDirectional.only(bottom: 50, end: 100),
+                    child: const Text(
+                      'Sign up or log in to see what\'s happening near you',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
 
-//             //'or' Seperator
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 Expanded(
-//                   child: Divider(
-//                     color: Colors.grey,
-//                     thickness: 1,
-//                     height: 10,
-//                   ),
-//                 ),
-//                 Padding(
-//                   padding: const EdgeInsets.symmetric(horizontal: 8.0),
-//                   child: Text(
-//                     'or',
-//                     style: TextStyle(
-//                       fontSize: 16,
-//                       color: Colors.grey,
-//                     ),
-//                   ),
-//                 ),
-//                 Expanded(
-//                   child: Divider(
-//                     color: Colors.grey,
-//                     thickness: 1,
-//                     height: 10,
-//                   ),
-//                 ),
-//               ],
-//             ),
+                  //Text fields for entering email and passowrd
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                    ),
+                    validator: emailValidator,
+                  ),
 
-//             //Sign in with Google Buttton
-//             SizedBox(height: 20),
-//             SizedBox(
-//               width: 300,
-//               child: ElevatedButton.icon(
-//                 onPressed: () async {
-//                   //Google sign in logic
-//                   GoogleSignIn googleSignIn = GoogleSignIn();
-//                   GoogleSignInAccount? account = await googleSignIn.signIn();
-//                   if (account != null) {
-//                     // Successful Google sign in
-//                   } else {
-//                     // Failed Google sign in
-//                   }
-//                 },
-//                 icon: Image.asset('assets/images/Google.png', height: 24),
-//                 label: Text('Continue with Google'),
-//               ),
-//             ),
-//             SizedBox(height: 10),
+                  TextFormField(
+                    controller: _passwordController,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter a password';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isPasswordHidden
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isPasswordHidden = !_isPasswordHidden;
+                          });
+                        },
+                      ),
+                    ),
+                    obscureText:
+                        _isPasswordHidden, // added option to hide/show password
+                  ),
 
-//             //Sign in with Facebook Buttton
-//             SizedBox(
-//               width: 300,
-//               child: ElevatedButton.icon(
-//                 onPressed: () async {
-//                   //Facebook sign in logic (API)
-//                   LoginResult result = await FacebookAuth.instance.login();
-//                   if (result.status == LoginStatus.success) {
-//                     // Successful Facebook sign in
-//                     String token = result.accessToken!.token;
-//                   } else {
-//                     // Failed Facebook sign in
-//                   }
-//                 },
-//                 icon: Image.asset('assets/images/Facebook.jpg', height: 24),
-//                 label: Text('Continue with Facebook'),
-//               ),
-//             ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TabsScreen(),
+                            ));
 
-//             //Don't have an account? Sign up! Gesture
-//             //Loads us to Sign up screen
-//             SizedBox(height: 20),
-//             GestureDetector(
-//               onTap: () {
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute(
-//                     builder: (context) => SignupScreen(),
-//                   ),
-//                 );
-//               },
-//               child: Text(
-//                 'Don\'t have an account? Sign up!',
-//                 style: TextStyle(
-//                   color: Colors.blue,
-//                   fontWeight: FontWeight.bold,
-//                 ),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+                        //Implement login API call
+                      }
+                    },
+                    child: const Text('LOGIN'),
+                  ),
+                  const SizedBox(height: 20),
+
+                  //'or' Seperator
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Expanded(
+                        child: Divider(
+                          color: Colors.grey,
+                          thickness: 1,
+                          height: 10,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Text(
+                          'or',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                      const Expanded(
+                        child: Divider(
+                          color: Colors.grey,
+                          thickness: 1,
+                          height: 10,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  //Sign in with Google Buttton
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: 300,
+                    child: ElevatedButton.icon(
+                      onPressed: () async {
+                        //Google sign in logic
+                        GoogleSignIn googleSignIn = GoogleSignIn();
+                        GoogleSignInAccount? account =
+                            await googleSignIn.signIn();
+                        if (account != null) {
+                          // Successful Google sign in
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TabsScreen(),
+                            ),
+                          );
+
+                          //Implement login API call
+                        } else {
+                          // Failed Google sign in
+                        }
+                      },
+                      icon: Image.asset('assets/images/Google.png', height: 24),
+                      label: const Text('Continue with Google'),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+
+                  //Sign in with Facebook Buttton
+                  SizedBox(
+                    width: 300,
+                    child: ElevatedButton.icon(
+                      onPressed: () async {
+                        //Facebook sign in logic (API)
+                        LoginResult result =
+                            await FacebookAuth.instance.login();
+                        if (result.status == LoginStatus.success) {
+                          // Successful Facebook sign in
+                          String token = result.accessToken!.token;
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TabsScreen(),
+                            ),
+                          );
+                        } else {
+                          // Failed Facebook sign in
+                        }
+                      },
+                      icon:
+                          Image.asset('assets/images/Facebook.jpg', height: 24),
+                      label: const Text('Continue with Facebook'),
+                    ),
+                  ),
+
+                  //Don't have an account? Sign up! Gesture
+                  //Loads us to Sign up screen
+                  const SizedBox(height: 20),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SignupScreen(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Don\'t have an account? Sign up!',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
