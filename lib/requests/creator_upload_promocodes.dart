6c544cdb55d91277913a0105e4fb211eb32eb42d
HossElbosss <84256353+HossElbosss@users.noreply.github.com>
2023-05-09 18:13:37 +0300

    reading from a csv file

---
+++ lib/requests/creator_upload_promocodes.dart
@@ -0,0 +1,115 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:file_picker/file_picker.dart';
+
+import '../providers/user_provider.dart';
+import 'routes_api.dart';
+
+Future<void> creatorUploadPromocodes(BuildContext context, String eventId) async {
+
+  String? userID = Provider.of<UserProvider>(context, listen: false).user.id;
+  String? token = Provider.of<UserProvider>(context, listen: false).token;
+
+  File? csv = await pickCsvFile();
+
+  if (csv != null) {
+    // print("csv obtained");
+    // List<int> csvBytes = await csv.readAsBytes();
+    //http.MultipartFile csvFile = http.MultipartFile.fromBytes('csvFile', csvBytes, filename: 'myfile.csv');
+
+
+  }
+
+  print(token);
+  final String baseUrl = '${RoutesAPI.checkPromo}/$userID/upload';
+  Uri url = Uri.parse(baseUrl);
+  final request = http.MultipartRequest('POST', url);
+  
+  final headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer $token'
+  };
+  //print(headers);
+  //print('Token: ${token}');
+  print(url);
+  try {
+    final response = await http.get(
+      url,
+      headers: headers,
+    );
+
+    print('Response: ${response.body}');
+    int responseStatus = response.statusCode;
+
+    if (responseStatus != 200) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: $responseStatus')),
+      );
+      return;
+    }
+    // Request permission to access the device's storage.
+    // final status = await Permission.storage.request();
+
+    // Get the directory path for storing files
+    final directory = await getExternalStorageDirectory();
+
+    if (directory != null) {
+      print(directory.path);
+      String filePath = directory.path + '/MyEvents.csv';
+
+      File file = File(filePath);
+      await file.writeAsBytes(response.bodyBytes);
+
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Success'),
+          content: Text('CSV file exported successfully'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+
+      return;
+      //}
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Failure'),
+          content: Text('Permission denied! Could not export CSV file'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
+    return;
+  } catch (error) {
+    print('Error while exporting: $error');
+    return;
+  }
+}
+
+Future<File?> pickCsvFile() async {
+  FilePickerResult? result = await FilePicker.platform.pickFiles(
+    type: FileType.custom,
+    allowedExtensions: ['csv'],
+  );
+
+  if (result != null && result.files.isNotEmpty) {
+    return File(result.files.single.path!);
+  } else {
+    return null;
+  }
+}
